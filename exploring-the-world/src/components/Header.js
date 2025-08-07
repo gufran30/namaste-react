2e5b59823d235ae6f6210ca1d7f12866b4cd3225
Gufran Khan <gufran3004@gmail.com>
2025-08-07 16:52:01 +0530

    episode 06 added

---
+++ exploring-the-world/src/components/Header.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+
+const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  console.log("header rendered");
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ exploring-the-world/src/utils/constants.js
@@ -0,0 +1,8 @@
+const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+const LOGO_URL =
+  "https://img.freepik.com/premium-vector/burger-logo-template_441059-18.jpg";
+
+export { CDN_URL, LOGO_URL };
+
